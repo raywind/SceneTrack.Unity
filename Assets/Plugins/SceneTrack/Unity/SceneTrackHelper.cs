e4cd2e86e0f9f7a586d750bef3c1ab6b0c3d0f7d
Robin Southern <betajaen@users.noreply.github.com>
2017-01-16 08:43:03 +0000

    SubmitArray functions and expanded GetTypeMemorySize
    Added plenty of SubmitArray functions to Helper, to prevent confusion.
    Added SubmitString function.
    GetTypeMemorySize covers all possible POD types supported by SceneTrack.

--- Assets/Plugins/SceneTrack/Unity/SceneTrackHelper.cs
+++ Assets/Plugins/SceneTrack/Unity/SceneTrackHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.CodeDom;
+﻿#define ST_FAST_COPY_MATRIX44
+
+using System;
+using System.CodeDom;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -10,21 +13,49 @@ namespace SceneTrack.Unity
     {
         public static uint GetTypeMemorySize(global::System.Type objectType, uint elements)
         {
-            if (objectType == typeof(float))
+            if (objectType == typeof(Single))
             {
-                return (uint) sizeof(float) * elements;
+                return (uint) sizeof(Single) * elements;
             }
-            else if (objectType == typeof(int))
+            else if (objectType == typeof(Double))
             {
-                return (uint) sizeof(int) * elements;
+                return (uint) sizeof(Double) * elements;
             }
-            else if (objectType == typeof(uint))
+            else if (objectType == typeof(Single))
             {
-                return (uint) sizeof(uint) * elements;
+                return (uint) sizeof(Single) * elements;
             }
-            else if (objectType == typeof(byte))
+            else if (objectType == typeof(Int64))
             {
-                return sizeof(byte) * elements;
+                return (uint) sizeof(Int64) * elements;
+            }
+            else if (objectType == typeof(UInt64))
+            {
+                return (uint) sizeof(UInt64) * elements;
+            }
+            else if (objectType == typeof(Int32))
+            {
+                return (uint) sizeof(Int32) * elements;
+            }
+            else if (objectType == typeof(UInt32))
+            {
+                return (uint) sizeof(UInt32) * elements;
+            }
+            else if (objectType == typeof(Int16))
+            {
+                return (uint) sizeof(Int16) * elements;
+            }
+            else if (objectType == typeof(UInt16))
+            {
+                return (uint) sizeof(UInt16) * elements;
+            }
+            else if (objectType == typeof(SByte))
+            {
+                return sizeof(SByte) * elements;
+            }
+            else if (objectType == typeof(Byte))
+            {
+                return sizeof(Byte) * elements;
             }
             return 0;
         }
@@ -33,5 +64,121 @@ namespace SceneTrack.Unity
         {
             return new Vector4(color.r,color.g, color.b, color.a);
         }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, byte[] array, uint stride)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_uint8(objectHandle, componentHandle, arrayPointer, (uint) array.Length, stride);
+            handle.Free();
+        }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, int[] array, uint stride)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_int32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, stride);
+            handle.Free();
+        }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, uint[] array, uint stride)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_uint32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, stride);
+            handle.Free();
+        }
+    
+        public static void SubmitArrayForceUInt32(uint objectHandle, uint componentHandle, int[] array, uint stride)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_int32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, stride);
+            handle.Free();
+        }
+
+        public static void SubmitArray(uint objectHandle, uint componentHandle, float[] array, uint stride)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, stride);
+            handle.Free();
+        }
+
+        public static void SubmitArray(uint objectHandle, uint componentHandle, Vector2[] array)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, sizeof(float) * 2);
+            handle.Free();
+        }
+
+        public static void SubmitArray(uint objectHandle, uint componentHandle, Vector3[] array)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, sizeof(float) * 3);
+            handle.Free();
+        }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, Vector4[] array)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, sizeof(float) * 4);
+            handle.Free();
+        }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, Color[] array)
+        {
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, sizeof(float) * 4);
+            handle.Free();
+        }
+    
+        public static void SubmitArray(uint objectHandle, uint componentHandle, Matrix4x4[] array)
+        {
+#if ST_FAST_COPY_MATRIX44
+            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            var arrayPointer = handle.AddrOfPinnedObject();
+            Object.SetValue_p_float32(objectHandle, componentHandle, arrayPointer, (uint) array.Length, sizeof(float) * 16);
+            handle.Free();
+#else
+            int arrayLength = array.Length;
+            float[] float16Matrix = new float[arrayLength * 16];
+
+            for(int i=0, j = 0;i < arrayLength;i++, j+=16)
+            {
+              Matrix4x4 m = array[i];
+              float16Matrix[j + 0]  = m.m00;
+              float16Matrix[j + 1]  = m.m01;
+              float16Matrix[j + 2]  = m.m02;
+              float16Matrix[j + 3]  = m.m03;
+              float16Matrix[j + 4]  = m.m10;
+              float16Matrix[j + 5]  = m.m11;
+              float16Matrix[j + 6]  = m.m12;
+              float16Matrix[j + 7]  = m.m13;
+              float16Matrix[j + 8]  = m.m20;
+              float16Matrix[j + 9]  = m.m21;
+              float16Matrix[j + 10] = m.m22;
+              float16Matrix[j + 11] = m.m23;
+              float16Matrix[j + 12] = m.m30;
+              float16Matrix[j + 13] = m.m31;
+              float16Matrix[j + 14] = m.m32;
+              float16Matrix[j + 15] = m.m33;
+            }
+
+            SubmitArray(objectHandle, componentHandle, float16Matrix, 16);
+
+            float16Matrix = null;
+#endif
+          }
+
+        public static void SubmitString(uint objectHandle, uint componentHandle, string value)
+        {
+          Object.SetValue_string(objectHandle, componentHandle, new StringBuilder(value), (uint) value.Length);
+        }
+    
     }
 }
